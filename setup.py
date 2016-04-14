81472f73c63b7e801a51166da563ca0b50868abd
Gerald <gera2ld@163.com>
2016-04-14 17:29:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!python
 # coding=utf-8
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pydns',
     version='1.0',
-    packages=['pydns'],
+    packages=find_packages(exclude=['test']),
     author='Gerald',
     author_email='i@gerald.top',
     description='DNS client and server written in Python, based on asyncio.',
