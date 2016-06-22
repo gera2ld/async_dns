01671f66ed12b606066c86805ed42786e7088a97
Gerald <gera2ld@163.com>
2016-06-22 20:40:12 +0800

    fix arguments

--- pydns/server.py
+++ pydns/server.py
@@ -53,7 +53,7 @@ def serve(host = '0.0.0.0', port = 53, protocol_classes = (DNSProtocol, DNSDatag
     DNSMixIn.resolver = aresolver.AsyncProxyResolver(resolve_protocol)
     if hosts is not None:
         DNSMixIn.resolver.cache.parse_file(hosts)
-    if proxies is not None:
+    if proxies:
         DNSMixIn.resolver.set_proxies(proxies)
     loop = asyncio.get_event_loop()
     logging.info('DNS server v2 - by Gerald')
@@ -80,8 +80,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'DNS server by Gerald.')
     parser.add_argument('-b', '--bind', default = ':', help = 'the address for the server to bind')
     parser.add_argument('--hosts', help = 'the path of a hosts file')
-    parser.add_argument('-p', '--proxy', default = '114.114.114.114,180.76.76.76,223.5.5.5,223.6.6.6', help = 'the proxy DNS servers')
-    parser.add_argument('-P', '--protocol', default='UDP', help = 'the default protocol to use to query remote servers')
+    parser.add_argument('-P', '--proxy', default = '114.114.114.114,180.76.76.76,223.5.5.5,223.6.6.6', help = 'the proxy DNS servers')
+    parser.add_argument('-p', '--protocol', default='UDP', help = 'the default protocol to use to query remote servers')
     args = parser.parse_args()
     host, _, port = args.bind.rpartition(':')
     if not host: host = '0.0.0.0'
