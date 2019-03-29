43285d50c2bc0b0e949389cbd9a6e4f9ad7f21a8
Michal Charemza <michal@charemza.name>
2019-03-29 21:47:30 +0000

    (refactor) Remove unused cache

--- async_dns/cache.py
+++ async_dns/cache.py
@@ -8,44 +8,6 @@ from . import logger, types, hosts, Record
 __all__ = ['DNSMemCache']
 CACHE_FILE = os.path.expanduser('~/.async_dns/named.cache.txt')
 
-def get_name_cache(
-        url='ftp://rs.internic.net/domain/named.cache',
-        filename=CACHE_FILE, timeout=10):
-    '''
-    Download root nameservers and save cache.
-    '''
-    from urllib import request
-    logger.info('Fetching named.cache...')
-    try:
-        res = request.urlopen(url, timeout=timeout)
-    except:
-        logger.warning('Error fetching named.cache')
-    else:
-        open(filename, 'wb').write(res.read())
-
-def get_root_servers(filename=CACHE_FILE):
-    '''
-    Load root servers from cache.
-    '''
-    if not os.path.isfile(filename):
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        get_name_cache(filename=filename)
-    # in case failed fetching named.cache
-    if not os.path.isfile(filename):
-        return
-    for line in open(filename, 'r'):
-        if line.startswith(';'):
-            continue
-        parts = line.split()
-        if len(parts) < 4:
-            continue
-        yield [
-            parts[0].rstrip('.'),   # name
-            # parts[1] (expires) is ignored
-            types.get_code(parts[2], 0),   # qtype
-            parts[3].rstrip('.'),   # data
-        ]
-
 class DNSMemCache(hosts.Hosts):
     '''
     Memory cache for DNS.
@@ -62,10 +24,3 @@ class DNSMemCache(hosts.Hosts):
         Add an item to cache.
         '''
         self.add_host(Record(name=name, data=data, qtype=qtype, ttl=-1))
-
-    def add_root_servers(self):
-        '''
-        Load root servers from cache file and add them to memory cache.
-        '''
-        for item in get_root_servers():
-            self.add_item(*item)
