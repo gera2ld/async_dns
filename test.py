c6f00f676796e598bc19450cf46a3bce893131c4
Michal Charemza <michal@charemza.name>
2019-03-29 20:41:14 +0000

    (refactor,tests) Allow test functions to be coroutines

--- test.py
+++ test.py
@@ -2,17 +2,25 @@ import asyncio
 import unittest
 
 from async_dns.resolver import (
-	ProxyResolver,
+    ProxyResolver,
 )
 from async_dns import (
-	types,
+    types,
 )
+
+
+def async_test(func):
+    def wrapper(*args, **kwargs):
+        future = func(*args, **kwargs)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(future)
+    return wrapper
 
 
 class TestResolver(unittest.TestCase):
 
-    def test_query(self):
-        loop = asyncio.get_event_loop()
+    @async_test
+    async def test_query(self):
         resolver = ProxyResolver()
-        res = loop.run_until_complete(resolver.query('www.baidu.com', types.A))
+        res = await resolver.query('www.baidu.com', types.A)
         self.assertTrue(res.an)
