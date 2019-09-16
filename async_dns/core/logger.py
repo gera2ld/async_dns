5f452ab1210df6aecff6632c3d98bc1a9e2aad2a
Gerald <gera2ld@163.com>
2019-09-16 13:14:46 +0800

    fix: ignore empty LOGLEVEL

--- async_dns/core/logger.py
+++ async_dns/core/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 logger = logging.getLogger(__package__)
-logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
+logger.setLevel(os.environ.get('LOGLEVEL') or 'INFO')
 handler = logging.StreamHandler()
 fmt = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 handler.setFormatter(fmt)
