5820d09cdc5ba9d83f0f7a44162954f3a0bb0ea4
Gerald <gera2ld@163.com>
2019-12-04 01:41:01 +0800

    fix: query cname from cache first

--- async_dns/resolver/query.py
+++ async_dns/resolver/query.py
@@ -21,12 +21,18 @@ class Query:
 
     async def query(self):
         error = None
+        domain = self.fqdn
+        nameservers = None
         try:
-            if not self.future.cancelled():
-                self.from_cache = await self.query_cache()
-                logger.debug('[query_cache][%s][%s] %s', types.get_name(self.qtype), self.fqdn, self.from_cache)
-            if not self.from_cache and not self.future.cancelled():
-                await self.query_remote()
+            while True:
+                if not self.future.cancelled():
+                    self.from_cache = await self.query_cache(domain)
+                    logger.debug('[query_cache][%s][%s] %s', types.get_name(self.qtype), domain, self.from_cache)
+                    if self.from_cache: break
+                if not self.future.cancelled():
+                    remote_res = await self.query_remote(domain, nameservers)
+                    if remote_res is None: break
+                    domain, nameservers = remote_res
         except Exception as e:
             error = e
         if not self.future.cancelled():
@@ -35,18 +41,18 @@ class Query:
             else:
                 self.future.set_result((self._result, self.from_cache))
 
-    async def query_cache(self):
+    async def query_cache(self, domain):
         '''Returns a boolean whether a cache hit occurs.'''
         resolver = self.resolver
         cache = resolver.cache
         result = self._result
         # if cached CNAME
-        cname = list(cache.query(self.fqdn, types.CNAME))
+        cname = list(cache.query(domain, types.CNAME))
         if cname:
-            result.an.extend(rec.copy(name=self.fqdn) for rec in cname)
+            result.an.extend(rec.copy(name=domain) for rec in cname)
             if all(rec.ttl < 0 for rec in cname):
                 result.aa = 1
-            if not resolver.recursive or self.qtype == types.CNAME:
+            if self.qtype == types.CNAME:
                 return True
             for rec in cname:
                 inter_res = await resolver.query(rec.data, self.qtype)
@@ -58,7 +64,7 @@ class Query:
             return True
         # else
         cache_hit = False
-        for rec in cache.query(self.fqdn, self.qtype):
+        for rec in cache.query(domain, self.qtype):
             if rec.qtype in (types.NS,):
                 inter_res = list(cache.query(rec.data, A_TYPES))
                 if inter_res:
@@ -67,75 +73,71 @@ class Query:
                     if rec.qtype == self.qtype:
                         cache_hit = True
             else:
-                result.an.append(rec.copy(name=self.fqdn))
+                result.an.append(rec.copy(name=domain))
                 if self.qtype == types.CNAME or rec.qtype != types.CNAME:
                     cache_hit = True
-        if any(self.fqdn.endswith(root) for root in self.resolver.rootdomains):
+        if any(domain.endswith(root) for root in self.resolver.rootdomains):
             if not cache_hit:
                 # not exists
                 result.r = 3
                 cache_hit = True
             # should only be added for domains that are resolved by this server
             result.aa = 1  # Authoritative answer
-            # result.ns.append(Record(name=fqdn, qtype=types.NS, data='localhost', ttl=-1))
-            # result.ar.append(Record(name=fqdn, qtype=types.A, data='127.0.0.1', ttl=-1))
+            # result.ns.append(Record(name=domain, qtype=types.NS, data='localhost', ttl=-1))
+            # result.ar.append(Record(name=domain, qtype=types.A, data='127.0.0.1', ttl=-1))
         return cache_hit
 
-    async def query_remote(self):
+    async def query_remote(self, domain, nameservers):
         '''Query domain from remote servers.'''
         resolver = self.resolver
         result = self._result
         has_result = False
-        fqdn = self.fqdn
-        nameservers = None
-        while not self.future.cancelled():
-            inter_res = await self.query_remote_once(fqdn, nameservers)
-            cname = []
-            has_ns = False
-            for rec in inter_res.an:
-                result.an.append(rec)
-                if rec.qtype == types.CNAME:
-                    cname.append(rec.data)
-                if self.qtype == types.CNAME or rec.qtype != types.CNAME:
-                    has_result = True
-            for rec in inter_res.ns:
-                if not resolver.recursive:
-                    result.ns.append(rec)
-                if rec.qtype == types.SOA or self.qtype == types.NS:
-                    has_result = True
-                else:
-                    has_ns = True
+        inter_res = await self.query_remote_once(domain, nameservers)
+        cname = []
+        has_ns = False
+        for rec in inter_res.an:
+            result.an.append(rec)
+            if rec.qtype == types.CNAME:
+                cname.append(rec.data)
+            if self.qtype == types.CNAME or rec.qtype != types.CNAME:
+                has_result = True
+        for rec in inter_res.ns:
             if not resolver.recursive:
-                result.ar.extend(inter_res.ar)
-            if has_result:
-                break
-            if cname:
-                fqdn = cname[0]
-                continue
-            if not resolver.recursive:
-                result.r = inter_res.r
-                break
-            if not has_ns:
-                result.r = 2
-                break
-            nsip_map = {}
-            for rec in inter_res.ar:
-                nsip_map[rec.name, rec.qtype] = rec.data
-            nsips = []
-            for rec in inter_res.ns:
-                host = rec.data.mname if rec.qtype == types.SOA else rec.data
-                ip = nsip_map.get((host, types.A))
-                if ip is not None:
-                    nsips.append(ip)
-            nameservers = NameServers(nsips)
+                result.ns.append(rec)
+            if rec.qtype == types.SOA or self.qtype == types.NS:
+                has_result = True
+            else:
+                has_ns = True
+        if not resolver.recursive:
+            result.ar.extend(inter_res.ar)
+        if has_result:
+            return
+        if cname:
+            return cname[0], None
+        if not resolver.recursive:
+            result.r = inter_res.r
+            return
+        if not has_ns:
+            result.r = 2
+            return
+        nsip_map = {}
+        for rec in inter_res.ar:
+            nsip_map[rec.name, rec.qtype] = rec.data
+        nsips = []
+        for rec in inter_res.ns:
+            host = rec.data.mname if rec.qtype == types.SOA else rec.data
+            ip = nsip_map.get((host, types.A))
+            if ip is not None:
+                nsips.append(ip)
+        return domain, NameServers(nsips)
 
-    async def query_remote_once(self, fqdn, nameservers=None):
+    async def query_remote_once(self, domain, nameservers=None):
         resolver = self.resolver
         req = DNSMessage(qr=REQUEST)
         if nameservers is None:
-            nameservers = resolver.get_nameservers(fqdn)
-        req.qd = [Record(REQUEST, fqdn, self.qtype)]
-        logger.debug('[query_remote][%s][%s] %s', types.get_name(self.qtype), fqdn, nameservers)
+            nameservers = resolver.get_nameservers(domain)
+        req.qd = [Record(REQUEST, domain, self.qtype)]
+        logger.debug('[query_remote][%s][%s] %s', types.get_name(self.qtype), domain, nameservers)
         inter_res = await self.request_remote(nameservers, req)
         resolver.cache_message(inter_res)
         return inter_res
