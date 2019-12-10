2a996b0029000336799bb850cc66e728d0621c32
Gerald <gera2ld@163.com>
2019-12-10 22:48:56 +0800

    1.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='async_dns',
-    version='1.0.4',
+    version='1.0.5',
     description='Asynchronous DNS client and server',
     long_description='Asynchronous DNS client and server written in pure Python, based on asyncio.',
     url='https://github.com/gera2ld/async_dns',
@@ -18,6 +18,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='async dns asyncio',
